d8976f4e550944eb1a04c01f63ed281b9954496c
Anderson Pereira <anderson.rasec@outlook.com>
2017-04-04 23:06:57 -0300

    Criada a classe Usuario.

---
+++ SiNoMA_MVC/Models/Usuario.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SiNoMA_MVC.Models
+{
+    public class Usuario
+    {
+        public int UsuarioID { get; set; }
+
+        [MaxLength(45)]
+        public string Login { get; set; }
+
+        [MaxLength(45)]
+        public string Senha { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime UltimoAcesso { get; set; }
+
+        [MaxLength(45)]
+        public string Email { get; set; }
+
+        public int Situacao { get; set; }
+
+        public int Administrador { get; set; }
+
+    }
+}
