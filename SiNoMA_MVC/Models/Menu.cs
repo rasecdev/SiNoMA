102063d83df63e4cb63dc17b3d4543a77516f377
Anderson Pereira <anderson.rasec@outlook.com>
2017-04-04 23:28:04 -0300

    Criada a classe Menu.

---
+++ SiNoMA_MVC/Models/Menu.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SiNoMA_MVC.Models
+{
+    public class Menu
+    {
+        public int MenuID { get; set; }
+
+        [MaxLength(45)]
+        [DataType(DataType.MultilineText)]
+        public string Descricao { get; set; }
+
+        [MaxLength(150)]
+        public string Url { get; set; }
+
+        [MaxLength(45)]
+        public string Icone { get; set; }
+
+        [MaxLength(45)]
+        public string Indice { get; set; }
+
+        public PermissaoAcesso PermissaoID { get; set; }
+
+        public Menu MenuPai { get; set; }
+    }
+}
