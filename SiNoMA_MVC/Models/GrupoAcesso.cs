8ba6506ce54f008220b0eaadbe45681b677e84a5
Anderson Pereira <anderson.rasec@outlook.com>
2017-04-04 23:10:21 -0300

    Criada a classe GrupoAcesso.

---
+++ SiNoMA_MVC/Models/GrupoAcesso.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SiNoMA_MVC.Models
+{
+    public class GrupoAcesso
+    {
+        public int GrupoID { get; set; }
+
+        [MaxLength(45)]
+        public string Descricao { get; set; }
+
+        public Usuario UsuarioID { get; set; }
+    }
+}
