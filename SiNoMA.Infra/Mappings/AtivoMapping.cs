999b0e7bcf772a207f259da39495e559243c1c77
Anderson Pereira <anderson.rasec@outlook.com>
2017-06-16 23:54:03 -0300

    Criação das classes de mapping para Usuário, Notificação e Ativo.

--- SiNoMA.Infra/Mappings/AtivoMapping.cs
+++ SiNoMA.Infra/Mappings/AtivoMapping.cs
@@ -1,10 +1,5 @@
 ﻿using SiNoMA_MVC.Models;
-using System;
-using System.Collections.Generic;
 using System.Data.Entity.ModelConfiguration;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SiNoMA.Infra.Mappings
 {
@@ -18,10 +13,10 @@ namespace SiNoMA.Infra.Mappings
             //Informa qual será a chave primária.
             HasKey(x => x.ID);
 
-            //Informa as características para a propriedade Nome, tem no máximo 60 caracteres e é obrigatória.            
+            //Informa as características para a propriedade ter no máximo 60 caracteres e é obrigatória.            
             Property(x => x.Nome).HasMaxLength(60).IsRequired();
 
-            //Informa as características para a propriedade Descrisao, tem no máximo 60 caracteres e é obrigatória.            
+            //Informa as características para a propriedade ter no máximo 60 caracteres e é obrigatória.            
             Property(x => x.Descrisao).HasMaxLength(60).IsRequired();
         }
     }
---
+++ SiNoMA.Infra/Mappings/UsuarioMapping.cs
@@ -0,0 +1,26 @@
+﻿using SiNoMA_MVC.Models;
+using System.Data.Entity.ModelConfiguration;
+
+namespace SiNoMA.Infra.Mappings
+{
+    class UsuarioMapping : EntityTypeConfiguration<Usuario>
+    {
+        public UsuarioMapping()
+        {
+            //Inforam qual será o nome da tabela.
+            ToTable("Usuario");
+
+            //Informa qual será a chave primária.
+            HasKey(x => x.ID);
+
+            //Informa as características para a propriedade ter no máximo 40 caracteres e é obrigatória.            
+            Property(x => x.Login).HasMaxLength(40).IsRequired();
+
+            //Informa as características para a propriedade ter no máximo 30 caracteres e é obrigatória.            
+            Property(x => x.Senha).HasMaxLength(30).IsRequired();
+
+            //Informa as características para a propriedade ter no máximo 40 caracteres e é obrigatória.            
+            Property(x => x.Email).HasMaxLength(40).IsRequired();
+        }
+    }
+}
