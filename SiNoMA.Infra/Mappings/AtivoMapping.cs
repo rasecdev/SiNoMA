3329e2da8248c2f4fd702c8331c9d09a32881804
Anderson Pereira <anderson.rasec@outlook.com>
2017-06-27 23:29:17 -0300

    Adição dos nomes das colunas na base de dados e formatação do código.

--- SiNoMA.Infra/Mappings/AtivoMapping.cs
+++ SiNoMA.Infra/Mappings/AtivoMapping.cs
@@ -13,11 +13,21 @@ namespace SiNoMA.Infra.Mappings
             //Informa qual será a chave primária.
             HasKey(x => x.ID);
 
-            //Informa as características para a propriedade ter no máximo 60 caracteres e é obrigatória.            
-            Property(x => x.Nome).HasMaxLength(60).IsRequired();
+            //Muda o nome do parâmetro na base de dados.
+            Property(x => x.ID)
+                .HasColumnName("ATIV_ID");
 
-            //Informa as características para a propriedade ter no máximo 60 caracteres e é obrigatória.            
-            Property(x => x.Descrisao).HasMaxLength(60).IsRequired();
+            //Informa as características para a propriedade ter no máximo 40 caracteres e é obrigatória e Muança no Nome.                       
+            Property(x => x.Nome)
+                .HasMaxLength(60)
+                .IsRequired()
+                .HasColumnName("ATIV_Nome");
+
+            //Informa as características para a propriedade ter no máximo 40 caracteres e é obrigatória e Muança no Nome.                       
+            Property(x => x.Descrisao)
+                .HasMaxLength(60)
+                .IsRequired()
+                .HasColumnName("ATIV_Descrisao"); ;
         }
     }
 }
